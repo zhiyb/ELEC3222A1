5fd2032a0baacc97f1e51c0d90988b20d012d5ee
Norman Zhi <yz39g13@soton.ac.uk>
2015-11-01 16:11:10 +0000

    PHY & DLL interface update

--- dll_layer.h
+++ dll_layer.h
@@ -20,9 +20,12 @@ uint8_t dll_read(uint8_t *data);
 
 // To be accessed by lower PHY layer
 
+// Sync byte required by PHY for performance reason
 static inline uint8_t dll_sync_byte() {return 0xaa;}
 
+// Receive data stream from PHY, called from ISR
 void dll_data_handler(const uint8_t data);
+// Transmit data stream request from PHY, called from ISR
 uint8_t dll_data_request(uint8_t *buffer);
 
 #ifdef __cplusplus
