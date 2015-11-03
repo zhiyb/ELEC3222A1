e8cf0c90df05943a72f33296fee9bb8ea1ce165f
Norman Zhi <yz39g13@soton.ac.uk>
2015-11-03 16:19:11 +0000

    DLL NET TRAN: interface update

--- dll_layer.h
+++ dll_layer.h
@@ -1,5 +1,5 @@
-#ifndef DLL_LAYER
-#define DLL_LAYER
+#ifndef DLL_LAYER_H
+#define DLL_LAYER_H
 
 #include <stdint.h>
 
@@ -7,20 +7,20 @@
 extern "C" {
 #endif
 
-// Data link layer: unreliable, in order, in unit of NET packets
+// Data link layer: communication of whole unit of packets
 
 // Initialisation
 void dll_init();
 
 // Interface with upper NET layer
 
-// Maximum packet length is defined by NET_PACKET_MAX_SIZE
-// Send out a packet
-void dll_send(uint8_t *data, uint8_t length);
-// Whether packets received
-void dll_available();
-// Take 1 packet, return the actual packet size
-uint8_t dll_read(uint8_t *data);
+// Maximum packet length: NET_PACKET_MAX_SIZE (128 bytes)
+// Buffer a packet and start transmission
+void dll_write(uint8_t *packet, uint8_t length);
+// Check if packets buffered ready for read
+uint8_t dll_available();
+// Take 1 packet from buffer, return the actual packet size read
+uint8_t dll_read(uint8_t *packet);
 
 // Interface with lower PHY layer
 
--- net_layer.h
+++ net_layer.h
@@ -1,14 +1,28 @@
 #ifndef NET_LAYER_H
 #define NET_LAYER_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void net_send(uint8_t length, char *buffer);
-// For unblocking operation, check if data available for receive
+// NET layer: machine routing, error control
+
+#define NET_PACKET_MAX_SIZE	128
+
+// Initialisation
+void net_init();
+
+// Interface with upper TRAN layer
+
+// Maximum packet length: TRAN_PACKET_MAX_SIZE (121 bytes)
+// Buffer a packet and start transmission
+void net_write(uint8_t *packet, uint8_t length);
+// Check if packets buffered ready for read
 uint8_t net_available();
-uint8_t net_recv(uint8_t length, char *buffer);
+// Take 1 packet from buffer, return the actual packet size read
+uint8_t net_read(uint8_t *packet);
 
 #ifdef __cplusplus
 }
