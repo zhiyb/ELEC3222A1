e716d0c47b3c2bfffe8394bd35df4b86d63aa3fe
Jiayang Sun <js9e13@soton.ac.uk>
2015-11-10 13:36:28 +0000

    app.cpp for test app layer

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 #include <stdio.h>
 #include <uart0.h>
 
+FILE *uart1;
+
 void init()
 {
 	uart0_init();
 	stdout = uart0_fd();
 	stdin = uart0_fd();
+	uart1 = uart1_fd();
 }
 
 int main()
 {
 	init();
 	puts("Initialised, hello world!");
+	fputs("Initialised, hello world!\n", uart1);
+	printf(uart1, "Initialised, hello world!\n");
 
 poll:
 	goto poll;
