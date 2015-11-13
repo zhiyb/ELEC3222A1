39768665aede6c05824cce7c22e7ef464e97cd87
Jiayang Sun <js9e13@soton.ac.uk>
2015-11-13 12:44:49 +0000

    TRAN: Second draft of tran.cpp

--- tran.cpp
+++ tran.cpp
@@ -4,8 +4,8 @@
 
 #define DATA_SIZE 114
 #define PORT 1
-char str[255];
-uint8_t *package
+char *str;
+uint8_t len;
 struct package{
 	uint8_t control[2];
 	uint8_t src_port;
@@ -13,129 +13,72 @@ struct package{
 	uint8_t length;
 	uint8_t app_data[DATA_SIZE];
 	uint16_t checksum;
-} pck
+} pck;
 
 struct socket{
 	uint16_t address;
 	uint16_t port;
 	uint16_t frame;
-} soc
-int main()
+} soc;
+
+int trans(void)
 {
-	get_soc();
-	pck.checksum = 0;
-redo:	if(sizeof(str) > DATA_SIZE)
+	//get_soc();
+	//get_data(*str, len);
+	//pck.checksum = 0;
+	//pack();
+	while(1)
 	{
-		if(sizeof(str) > 2 * DATA_SIZE)
-		{
-			for(uint8_t i = 0; i < 3; i++)
-			{
-				pck.control[1] = i;
-				pck.control[2] = (i == 2);	
-				if(i = 2)
-				{
-					for(uint8_t j = (2 * DATA_SIZE - 1); j < sizeof(str); j++)
-					{
-						pck.app_data[j + 1 - 2 * DATA_SIZE] = str[j];
-						pck.checksum += str[j];
-					}
-					//pck.src_port = PORT;
-					//pck.dest_port = soc.port;
-					pck.length = sizeof(pck.app_data);
-					pck.checksum += pck.src_port;
-					pck.checksum += pck.dest_port;
-					pck.checksum += pck.length;
-					pck.checksum += pck.control[1];
-					pck.checksum += pck.control[2];
-					//send_net(&pck);					
-				}
-				else
-				{
-					for(uint8_t j = 0; j < DATA_SIZE; j++)
-					{
-						pck.app_data[j] = str[j];
-						pck.checksum += str[j];
-					}
-					pck.src_port = PORT;
-					pck.dest_port = soc.port;
-					pck.length = DATA_SIZE;
-					pck.checksum += pck.src_port;
-					pck.checksum += pck.dest_port;
-					pck.checksum += pck.length;
-					pck.checksum += pck.control[1];
-					pck.checksum += pck.control[2];
-					//send_net(&pck);					
-				}
-				pck.checksum = 0;
-			}	
-		}
-		else
-		{
-			for(uint8_t i = 0; i < 2; i++)
-			{
-				pck.control[1] = i;
-				pck.control[2] = (i == 1);	
-				if(i = 1)
-				{
-					for(uint8_t j = (DATA_SIZE - 1); j < sizeof(str); j++)
-					{
-						pck.app_data[j + 1 - DATA_SIZE] = str[j];
-						pck.checksum += str[j];
-					}
-					//pck.src_port = PORT;
-					//pck.dest_port = soc.port;
-					pck.length = sizeof(pck.app_data);
-					pck.checksum += pck.src_port;
-					pck.checksum += pck.dest_port;
-					pck.checksum += pck.length;
-					pck.checksum += pck.control[1];
-					pck.checksum += pck.control[2];
-					//send_net(&pck);					
-				}
-				else
-				{
-					for(uint8_t j = 0; j < DATA_SIZE; j++)
-					{
-						pck.app_data[j] = str[j];
-						pck.checksum += str[j];
-					}
-					pck.src_port = PORT;
-					pck.dest_port = soc.port;
-					pck.length = DATA_SIZE;
-					pck.checksum += pck.src_port;
-					pck.checksum += pck.dest_port;
-					pck.checksum += pck.length;
-					pck.checksum += pck.control[1];
-					pck.checksum += pck.control[2];
-					//send_net(&pck);					
-				}
-				pck.checksum = 0;
-			}				
-		}
+		get_soc();
+		pack();
 	}
-	else
+	return 1;	
+}
+void pack(void)
+{
+	pck.src_port = PORT;
+	pck.dest_port = soc.dest_port;
+	for(uint8_t i = 0; i < len / DATA_SIZE + 1; i++)
 	{
+		pck.checksum = 0;
 		pck.control[1] = i;
-		pck.control[2] = (i == 2);	
-		if(i = 2)
+		pck.control[2] = (i == len / DATA_SIZE);
+		if(pck.control[2] == 1)
 		{
-			for(uint8_t j = (2 * DATA_SIZE - 1); j < sizeof(str); j++)
+			for(uint8_t j = i * DATA_SIZE; j < len; j++)
 			{
-				pck.app_data[j + 1 - 2 * DATA_SIZE] = str[j];
+				uint8_t tem = 0;
+				pck.appdata[tem] = str[j];
 				pck.checksum += str[j];
 			}
 			//pck.src_port = PORT;
 			//pck.dest_port = soc.port;
-			pck.length = sizeof(pck.app_data);
+			pck.length = len - (i * DATA_SIZE);
 			pck.checksum += pck.src_port;
 			pck.checksum += pck.dest_port;
 			pck.checksum += pck.length;
-			pck.checksum += pck.control[1];
-			pck.checksum += pck.control[2];
-			//send_net(&pck);					
+			pck.checksum += control[1];
+			pck.checksum += control[2];
+			//send
+			//pck.checksum = 0;
 		}
-
+		else
+		{
+			for(uint8_t j = i * DATA_SIZE; j < (i + 1) * DATA_SIZE; j++)
+			{
+				uint8_t tem = 0;
+				pck.appdata[tem] = str[j];
+				pck.checksum += str[j];
+			}
+			//pck.src_port = PORT;
+			//pck.dest_port = soc.port;
+			pck.length = DATA_SIZE;
+			pck.checksum += pck.srcport;
+			pck.checksum += pck.dest_port;
+			pck.checksum += pck.length;
+			pck.checksum += control[1];
+			pck.checksum += control[2];
+			//send
+		}
 	}
-	goto redo;
-	return 1;	
 }
