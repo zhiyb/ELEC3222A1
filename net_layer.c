9167d0368b89185c313c28ee717a5e4095c308ad
mengjiuxi <jm15g13@soton.ac.uk>
2015-11-19 17:17:51 +0000

    change commit style

---
+++ net_layer.c
@@ -0,0 +1,61 @@
+#include "net_layer.h"
+#include "dll_lyaer.h"
+#include <studio.h>
+
+//--------function call always from top to bottom.--------//
+//														  //
+//				 TRAN call NET call DLL 				  //
+//--------------------------------------------------------//
+
+static union net_buffer{
+	struct{
+			data[126];
+	}d;
+	struct {
+		unit16_t control;
+		unit8_t SRC_address;
+		unit8_t DEST_Address;
+		unit8_t Length;
+		unit8_t TRAN_Segement[0];
+	} s;
+}Txbuffert;
+
+static union net_buffer{
+	struct{
+			data[126];
+	}d;
+	struct {
+		unit16_t control;
+		unit8_t SRC_address;
+		unit8_t DEST_Address;
+		unit8_t Length;
+		unit8_t TRAN_Segement[0];
+	} s;
+}Rxbuffert;
+
+#define Checksum(st) ((unit16_t * )((st).s.data + (st).s.length))
+
+
+void net_write(const uint8_t *packet, uint8_t length)
+{
+	Txbuffert.s.control
+	packet = control + SRC_address + DEST_Address + Length + packet + Checksum;
+
+	dll_write(*packet, legth);
+}
+
+uint8_t net_read(uint8_t *packet)
+{
+	//define a another buffer here and pass it to dll_read(*packet)
+	if (dll_read(*packet1))
+		{
+			*packet = packet1.[TRAN_Segement]
+			return 1;
+		}
+	else return 0;
+}
+
+void net_init()
+{
+	;
+}
