4ac5d3c43ae37a4c346dd99a1b32621aefb3fcd4
Norman Zhi <normanzyb@gmail.com>
2015-11-21 09:54:01 +0000

    LLC: interface update

--- llc_layer.h
+++ llc_layer.h
@@ -14,26 +14,34 @@ extern "C" {
 
 // Logical link control layer: transmission in unit of packets
 
-#define LLC_FRAME_MAX_SIZE	28
+#define LLC_FRAME_MAX_SIZE	26
 
 // Initialisation
 void llc_init();
 
 // Interface with upper NET layer
 
-enum LLC_Primitives {DL_UNITDATA = 0, DL_DATA_ACK, DL_DATA_ACK_STATUS};
+// Unacknowledged packet, acknowledged packet
+enum LLC_Primitives {DL_UNITDATA = 0, DL_DATA_ACK};
 
 // Data transfer between LLC and upper layer
-struct llc_packet {
+struct llc_packet_t {
 	uint8_t pri;	// LLC_Primitives
+	uint8_t addr;	// Destination MAC address
 	uint8_t len;	// Data length
-	void *ptr;	// Pointer to data (free after use)
+	void *ptr;	// Pointer to frame data on heap (free after use)
+	void *payload;	// Pointer to payload field
 };
 
 // RTOS queue for packet transmission & reception
 // Queue item size: llc_packet
-extern QueueHandle_t llc_tx, llc_rx;
+extern QueueHandle_t llc_rx;
 
+// Transmit a packet
+// The data pointed by ptr will not be freed
+// Return:	DL_UNITDATA: always 1
+// 		DL_DATA_ACK: acknowledged or not
+uint8_t llc_tx(uint8_t pri, uint8_t addr, uint8_t len, void *ptr);
 uint8_t llc_written();
 // Copy data to tx queue
 void llc_write(void *data, uint8_t length);
